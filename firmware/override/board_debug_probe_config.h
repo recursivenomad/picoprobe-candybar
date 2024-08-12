90d7886adea9c04cecdb72c909f25a862cee1cd4
Peter S. Hollander <recursivenomad@protonmail.com>
2024-08-11 23:07:54 -0400

    Add unconnected UART hardware flow control

--- firmware/override/board_debug_probe_config.h
+++ firmware/override/board_debug_probe_config.h
@@ -25,14 +25,23 @@
 #define OVERRIDE_BOARD_DEBUG_PROBE_H_
 
 
-// Depend on the original Debug Probe configuration to maintain near-parity
+// Depend on the original Debug Probe configuration to maintain a superset
 #include "../lib/debugprobe/include/board_debug_probe_config.h"
 
 #include "boards/pico.h"
 
 
+#define PROBE_UART_HWFC
+
+
 #undef  PROBE_PIN_RESET
 #define PROBE_PIN_RESET 3
+
+#ifdef PROBE_UART_HWFC
+  #define PROBE_UART_DTR  9
+  #define PROBE_UART_CTS 10
+  #define PROBE_UART_RTS 11
+#endif
 
 #undef  PROBE_USB_CONNECTED_LED
 #define PROBE_USB_CONNECTED_LED PICO_DEFAULT_LED_PIN
